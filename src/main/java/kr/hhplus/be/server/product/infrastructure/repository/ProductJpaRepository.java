eac189fcf9d328ba44a667bfc056ab0a6661e0da
sky96027 <93393540+sky96027@users.noreply.github.com>
2025-08-04 14:58:21 +0900

    FEAT : Order+items, Product+options 쿼리 추가

--- src/main/java/kr/hhplus/be/server/product/infrastructure/repository/ProductJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/product/infrastructure/repository/ProductJpaRepository.java
@@ -2,8 +2,13 @@ package kr.hhplus.be.server.product.infrastructure.repository;
 
 import kr.hhplus.be.server.product.infrastructure.entity.ProductJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductJpaRepository extends JpaRepository<ProductJpaEntity, Long> {
+    @Query("SELECT DISTINCT p FROM ProductJpaEntity p JOIN FETCH p.options")
+    List<ProductJpaEntity> findAllWithOptions();
 }
