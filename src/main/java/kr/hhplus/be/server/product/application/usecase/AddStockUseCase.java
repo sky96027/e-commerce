953ee082ab1def87240e969a3aa3a9c878d2ce9a
sky96027 <93393540+sky96027@users.noreply.github.com>
2025-08-06 00:10:36 +0900

    REFACTOR : 결제 실패 시 복구 로직 추가

---
+++ src/main/java/kr/hhplus/be/server/product/application/usecase/AddStockUseCase.java
@@ -0,0 +1,20 @@
+package kr.hhplus.be.server.product.application.usecase;
+
+/**
+ * [UseCase - Port In]
+ * 상품 재고를 복구시키는 유스케이스에 대한 추상 정의.
+ *
+ * 이 인터페이스는 애플리케이션 계층에서
+ * 재고를 증가시키는 기능을 외부(presentation, facade 등)에 노출하기 위한 계약(Contract)이다.
+ *
+ * 구현체는 service 패키지 내에서 정의되며,
+ * presentation 계층은 이 인터페이스만 의존함으로써 구현체에 대한 결합을 피할 수 있다.
+ */
+public interface AddStockUseCase {
+    /**
+     * 옵션 ID와 판매량을 받아 재고를 증가시킨다.
+     * @param optionId 옵션 ID
+     * @param quantity 증가량
+     */
+    void addStock(long optionId, int quantity);
+}
