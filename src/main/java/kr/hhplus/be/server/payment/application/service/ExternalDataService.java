cfa30b6d74e49951f4be2485d470cb5fda2264a1
sky96027 <93393540+sky96027@users.noreply.github.com>
2025-08-28 07:44:39 +0900

    FEAT : 외부 데이터 플랫폼 전송 추가

---
+++ src/main/java/kr/hhplus/be/server/payment/application/service/ExternalDataService.java
@@ -0,0 +1,10 @@
+package kr.hhplus.be.server.payment.application.service;
+
+import kr.hhplus.be.server.payment.application.usecase.ExternalDataUseCase;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExternalDataService implements ExternalDataUseCase {
+    @Override
+    public void send(){}
+}
